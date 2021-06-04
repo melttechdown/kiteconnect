693860ca3829c96ca71e431bc70d47d5fa3f2856
melttechdown <melttechdown@gmail.com>
2021-06-05 00:57:49 +0400

    InitialCommit
    This is the first skeleton commit

---
+++ src/main/java/com/app/kiteconnect/KiteconnectApplication.java
@@ -0,0 +1,13 @@
+package com.app.kiteconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiteconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiteconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/kiteconnect/KiteconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.kiteconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiteconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
